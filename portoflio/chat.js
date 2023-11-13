9c3eee37b559377d661517c23ca87c05253d9aaa
Megin <Meginvanherk@gmail.com>
2023-11-13 13:37:21 +0100

    chatbotfucked

--- portoflio/chat.js
+++ portoflio/chat.js
@@ -2,21 +2,30 @@ async function sendMessage() {
     const userInput = document.getElementById("message-input").value;
     addMessage(userInput, 'outgoing');
 
-    // Send user input to the server
-    const response = await fetch('/get_response', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message: userInput }),
-    });
-
-    const data = await response.json();
-    const botResponse = data.message;
+    // Simulate getting a response from the JavaScript file
+    const botResponse = getBotResponse(userInput);
     addMessage(botResponse, 'incoming');
 
     // Clear the input field
     document.getElementById("message-input").value = "";
+}
+
+function getBotResponse(userInput) {
+    // Simulate getting responses from the JavaScript file
+    const responses = {
+        hello: ['Hi there!', 'Hello!', 'Hey!'],
+        'how are you': ["I'm good, thanks!", "I'm doing well, how about you?", "I'm great!"],
+        "what's your name": ["I'm a chatbot.", "Call me ChatMaster.", "I don't have a name. You can call me anything."],
+        bye: ['Goodbye!', 'See you later!', 'Bye!'],
+    };
+
+    for (const key in responses) {
+        if (userInput.toLowerCase().includes(key)) {
+            return responses[key][Math.floor(Math.random() * responses[key].length)];
+        }
+    }
+
+    return "I'm not sure how to respond to that.";
 }
 
 function addMessage(content, type) {
@@ -25,4 +34,14 @@ function addMessage(content, type) {
     messageDiv.className = `message ${type}`;
     messageDiv.innerHTML = `<span class="message-content">${content}</span>`;
     messagesContainer.appendChild(messageDiv);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight; // Auto-scroll to the bottom
 }
+
+
+  // Event listener for Enter key press
+  document.getElementById('message-input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      sendMessage();
+    }
+  });
+  
--- portoflio/index.js
+++
@@ -1,8 +0,0 @@
-
-  // Event listener for Enter key press
-  document.getElementById('message-input').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      sendMessage();
-    }
-  });
-  
